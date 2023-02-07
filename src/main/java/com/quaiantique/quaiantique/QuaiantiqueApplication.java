890a652601b3965e388b223ef0d9fcd3cf3d049f
Fabien Sabri <fabien.sabri@niji.fr>
2023-02-07 14:43:25 +0100

    Init spring commit

---
+++ src/main/java/com/quaiantique/quaiantique/QuaiantiqueApplication.java
@@ -0,0 +1,13 @@
+package com.quaiantique.quaiantique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuaiantiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuaiantiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quaiantique/quaiantique/QuaiantiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quaiantique.quaiantique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuaiantiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
