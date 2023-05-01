0d89e3c18d774dc57b224e42b4d98a5bfaf377b3
Fabien Sabri <fabien.sabri@niji.fr>
2023-05-01 19:03:31 +0200

    clean

--- src/main/java/com/quaiantique/quaiantique/DAO/ImageDAO.java
+++
@@ -1,8 +0,0 @@
-package com.quaiantique.quaiantique.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.quaiantique.quaiantique.Entities.Image;
-
-public interface ImageDAO extends JpaRepository<Image, String> {
-    
-}
